c1d39fd77bb9acb665708fffa9fb69cf817654b8
Shreewatsa Timalsena <shreewatsatimalsena@lftechnology.com>
2022-09-03 17:45:00 +0545

    [fix] minor fix

--- .config/commons/aliases.sh
+++ .config/commons/aliases.sh
@@ -30,7 +30,7 @@ alias lg="lazygit"
 alias bare="git --git-dir=$HOME/dotfiles --work-tree=$HOME"
 alias barels="bare ls-files --full-name"
 alias barelsl="bare ls-tree --full-tree -r HEAD --name-only"  # Do not use this => bare log --pretty=format: --name-only --diff-filter=A | sort -;  
-alias baretree="barelsl | tree -C --fromfile . | ${PAGER:-less};"  # View tracked files in a tree.
+alias baretree="barelsl | tree -Ca --fromfile . | ${PAGER:-less};"  # View tracked files in a tree.
 alias barefind='nvim $(barelsl | fzf | sed "s,^,$(bare rev-parse --show-toplevel)/,")'  # Open tracked file in nvim.
 
 # alias vim="nvim"
